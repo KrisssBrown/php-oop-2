3a14a9ce702432c2f5b76d634e9db59715d96506
KrisssBrown <cristianbruno.cs@gmail.com>
2023-04-03 23:51:41 +0200

    EDIT

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <?php
 
 require_once "./models/products.php";
-require_once "./models/cat.php";
-require_once "./models/dog.php";
---
+++ models/product.php
@@ -0,0 +1,9 @@
+<?php
+
+
+class Product {
+    protected string $cibo;
+    protected string $giochi;
+    protected string $cucce;
+    
+}
